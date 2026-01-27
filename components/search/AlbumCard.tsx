8e2d9816c4d5dc8898d1d6d6e850db8d7e906e5d
nainish-rai <nainishraisjp@gmail.com>
2026-01-27 13:31:05 +0530

    fix: remove extra onError handler from AlbumCard to match spec

--- components/search/AlbumCard.tsx
+++ components/search/AlbumCard.tsx
@@ -66,10 +66,6 @@ export default function AlbumCard({ album }: AlbumCardProps) {
               className="object-cover"
               priority={false}
               loading="lazy"
-              onError={(e) => {
-                const target = e.target as HTMLImageElement;
-                target.src = "/placeholder-album.png";
-              }}
             />
 
             {/* Hover Overlay with Play Button */}
